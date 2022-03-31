06a6473b8f89370cef0a1992dd6d6333c4f633ae
hojun <84840627+hojunking@users.noreply.github.com>
2022-03-31 15:17:57 +0900

    update DS0331

---
+++ program_using_stack1/1/1.c
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_STACK_SIZE 100
+#define MAX_EXPR_SIZE 80
+/*
+	후위표기식 연산 프로그램
+*/
+
+
+typedef enum 
+{
+	lparen,rparen,plus, minus,times, divide,
+	mod,eos,operand
+} precedence;
+
+int stack[MAX_STACK_SIZE];
+char expr[MAX_EXPR_SIZE];
+int top;
+
+
+void push(int item);
+int pop();
+precedence getToken(char* symbol, int* n);
+int eval(void);
+
+void push(int item) {
+	stack[++top] = item;
+	return;
+}
+int pop() {
+	return stack[top--];
+}
+
+int main(void) {
+	FILE* fp;
+	FILE* fpw;
+	char ch;
+	if (fopen_s(&fpw, "output.txt", "w"))
+		exit(1);
+
+	if (fopen_s(&fp, "input.txt", "r"))
+		exit(1);
+	printf("postfix expression : ");
+	int a = 0;
+	fputs("postfix expression : ",fpw);
+	while (EOF != (ch = fgetc(fp))) {
+		expr[a++] = ch;
+		fputc(ch, stdout);
+		fputc(ch, fpw);
+	}
+	fclose(fp);
+	
+	char result = eval() + '0';
+	printf("\nthe evaluation value : %c", result);
+	fputs("\nthe evaluation value : ", fpw);
+	fputc(result, fpw);
+	
+	fclose(fpw);
+}
+
+int eval(void) {
+	precedence token;
+
+	char symbol;
+	int op1, op2;
+	int n = 0;
+	top = -1;
+
+	token = getToken(&symbol, &n);
+	while (token != eos) {
+		if (token == operand)
+			push(symbol - '0');
+		else {
+			op2 = pop();
+			op1 = pop();
+			switch (token) {
+			case plus: push(op1 + op2);
+				break;
+			case minus: push(op1 - op2);
+				break;
+			case times: push(op1 * op2);
+				break;
+			case divide: push(op1 / op2);
+				break;
+			case mod: push(op1 % op2);
+			}
+		}
+		token = getToken(&symbol, &n);
+	}
+	return pop();
+}
+
+precedence getToken(char* symbol, int* n) {
+	*symbol = expr[(*n)++];
+	switch (*symbol) {
+	case '(': return lparen;
+	case ')': return rparen;
+	case '+': return plus;
+	case '-': return minus;
+	case '*': return times;
+	case '/': return divide;
+	case '%': return mod;
+	case '\0': return eos;
+	default: return operand;
+	}
+}
---
+++ program_using_stack1/2/2.c
@@ -0,0 +1,128 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+    중위표기법 -> 후위표기로 변환
+    및 연산 프로그램
+    
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE_STACK 100
+#define MAX_EXPR_SIZE 80
+#define FALSE 0
+#define TRUE 0
+char expr[MAX_EXPR_SIZE];
+int stack[MAX_SIZE_STACK];
+int top;
+
+void init() {
+    top = -1;
+}
+
+int is_empty() {
+    return top == -1;
+}
+
+int is_full() {
+    return top == MAX_SIZE_STACK - 1;
+}
+
+void push(int item) {
+    if (is_full())
+        exit(1);
+    stack[++top] = item;
+}
+
+int pop( s) {
+    if (is_empty(s))
+        exit(1);
+    return stack[top--];
+}
+
+int peek( s) {
+    if (is_empty(s))
+        exit(1);
+    return stack[top];
+}
+
+int prec(char op) {
+    switch (op) {
+    case '(': case ')': return 0;
+    case '+': case '-': return 1;
+    case '*': case '/': return 2;
+    }
+    return -1;
+}
+
+void postfix(char exp[]) {
+    FILE* fpw;
+    if (fopen_s(&fpw, "output.txt", "w"))
+        exit(1);
+    int i;
+    char ch, top;
+    int len = (int)strlen(exp);
+    init();
+
+    for (i = 0; i < len; i++) {
+        ch = (int)exp[i];
+
+        switch (ch) {
+        case '+': case '-': case '*': case '/':
+            while (!is_empty() && prec(peek()) >= prec(ch)) {
+                top = pop();
+                printf("%c", top);
+                fputc(top, fpw);
+            }
+            push(ch);
+            break;
+
+        case '(':
+            push(ch);
+            break;
+        case ')':
+            top = pop();
+            while (top != '(') {
+                printf("%c", top);
+                fputc(top, fpw);
+                top = pop();
+            }
+            break;
+
+        default:
+            printf("%c", ch);
+            fputc(ch, fpw);
+            break;
+        }
+    }
+
+    while (!is_empty()) {
+        char temp = pop();
+        printf("%c", temp);
+        fputc(temp, fpw);
+
+    }
+    fclose(fpw);
+}
+
+int main() {
+    FILE* fp;
+    
+    
+
+    char ch;
+    if (fopen_s(&fp, "input.txt", "r"))
+        exit(1);
+    printf("<<<<<<<< infix to posfix >>>>>>>>\n");
+    printf("infix expression   :");
+    int a = 0;
+    
+    while (EOF != (ch = fgetc(fp))) {
+        expr[a++] = ch;
+        fputc(ch, stdout);
+    }
+    printf("\npost expression   :");
+    postfix(expr);
+    fclose(fp);
+    return 0;
+}
