46e9125ad8a6d8c56b40362b4f2258273b71dc5e
hojun <84840627+hojunking@users.noreply.github.com>
2022-05-29 22:31:02 +0900

    practice HEAP

--- binaryTree_max_min_heap/4/prac_min_heap.c
+++ binaryTree_max_min_heap/4/prac_min_heap.c
@@ -1,67 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define MAX_SIZE 100
-#define HEAP_FULL(n) (n == MAX_SIZE -1)
+#define MAX 20
+#define HEAP_FULL(n) (n == MAX-1)
 #define HEAP_EMPTY(n) (!n)
 
-typedef struct node {
-	int key;
-}node;
 
-node heap[MAX_SIZE];
+typedef struct element {
+	int key;
+} element;
+
+element heap[MAX];
 int n;
 
-void heap_push(int item) {
-	if (HEAP_FULL(++n)) exit(1);
-	int i = n; //last node of heap
+void push(int data) {
+	int i;
+	if (HEAP_FULL(n)) exit(1);
 
-	while ((i != 1) && (item < heap[i / 2].key)) {
+	i = ++n;
+	while ((i != 1) && (data < heap[i / 2].key)) {
 		heap[i] = heap[i / 2];
 		i /= 2;
 	}
-	heap[i].key = item;
+	heap[i].key = data;
 }
 
-int heap_pop() {
+int pop() {
 	if (HEAP_EMPTY(n)) exit(1);
+
 	int parent = 1, child = 2;
-	node tmp = heap[n--];
-	node item = heap[1];
-	
+	element tmp = heap[--n];
+	element data = heap[1];
+
 	while (child <= n) {
 		if ((child < n) && (heap[child].key > heap[child + 1].key))
 			child++;
-		if (tmp.key <= heap[child].key) break;
+		if (tmp.key < heap[child].key)break;
 		heap[parent] = heap[child];
 		parent = child;
 		child *= 2;
 	}
-	heap[parent] = tmp;
-	return item.key;
-}
+	heap[parent].key = tmp.key;
 
-int main() {
+	return data.key;
+
+}
+int main(void) {
 	FILE* fp;
 	if (fopen_s(&fp, "input.txt", "r"))
 		exit(1);
-	
-	n = 0; //init_heap
-	int tmp;
-	
-	printf("***** insertion into a min heap *****\n");
-	while (fscanf_s(fp, "%d", &tmp) != EOF) {
-		heap_push(tmp);
-		for (int i = 1; i <= n; i++) 
+	int t;
+	printf("***** insertion into a max heap *****\n");
+	while (fscanf_s(fp, "%d ", &t) != EOF) {
+		push(t, n);
+		for (int i = 1; i <= n; i++) {
 			printf("%2d ", heap[i].key);
+		}
 		printf("\n");
 	}
-	tmp = n;
-	printf("***** deletion from a min heap *****\n");
-	for (int i = 0; i <= tmp; i++) {
-		printf("delete data = %d\n", heap_pop());
-		for (int j = 1; j <= n; j++)
-			printf("%2d ", heap[j].key);
+	printf("***** deletion from a max heap *****\n");
+	t = n;
+	int del;
+	for (int i = 1; i <= t; i++) {
+		del = pop();
+		printf("delete data = %d\n", del);
+		for (int i = 1; i <= n; i++) {
+			printf("%2d ", heap[i].key);
+		}
 		printf("\n");
 	}
-	return 0;
 }
--- graph_DFS_BFS/practice_adjgraph_DFS_BFS/practice_adjgraph_DFS_BFS.c
+++ graph_DFS_BFS/practice_adjgraph_DFS_BFS/practice_adjgraph_DFS_BFS.c
@@ -1,25 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINIGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_SIZE 100
-#define ISFULL(x) (x == MAX_SIZE-1)
-#define ISEMPTY(x) (!x)
+#define MAX 50
+#define ISFULL(x) (x == MAX-1)
 
 typedef struct node* link;
 typedef struct node {
 	int vertax;
 	link path;
 } node;
+int n, l;
 
-int n, l, rear, front;
-int visited[MAX_SIZE], queue[MAX_SIZE];
-link graph[MAX_SIZE] = { NULL };
+link graph[MAX] = { NULL };
+int visited[MAX], queue[MAX];
+int rear, front;
 
 void adjList(int v, int e) {
 	link nNode = (link)malloc(sizeof(node));
 	if (!nNode) exit(1);
-	
+
 	nNode->vertax = e;
 	nNode->path = graph[v];
 	graph[v] = nNode;
@@ -27,30 +27,20 @@ void adjList(int v, int e) {
 
 void printList() {
 	for (int i = 0; i < n; i++) {
-		printf("Graph[%d] : ", i);
 		link tmp = graph[i];
+		printf("graph[%d] : ", i);
 		while (tmp) {
-			printf(" %d ", tmp->vertax);
+			printf("%d ", tmp->vertax);
 			tmp = tmp->path;
 		}
 		printf("\n");
 	}
 }
-void push(int v) {
-	if(ISFULL(++rear)) exit(1);
-	queue[rear] = v;
-}
-
-int pop() {
-	if (front == rear) exit(1);
-	return queue[++front];
-}
 
 void DFS(int v) {
 	visited[v] = 1;
 	link tmp = graph[v];
-	printf("%d ", v);
-
+	printf("%d  ", v);
 	while (tmp) {
 		if (!visited[tmp->vertax])
 			DFS(tmp->vertax);
@@ -58,33 +48,45 @@ void DFS(int v) {
 	}
 }
 
+void push(int v) {
+	if (ISFULL(rear)) exit(1);
+	queue[++rear] = v;
+}
+
+int pop() {
+	if (front == rear) exit(1);
+	return queue[++front];
+}
+
+
 void BFS(int v) {
-	rear = front = -1;
+	memset(visited, 0, MAX);
+	front = rear = -1;
 	visited[v] = 1;
-	link tmp = graph[v];
-	printf("%d ", v);
+	printf("%d  ", v);
 	push(v);
 	while (rear != front) {
 		v = pop();
-		tmp = graph[v];
+		link tmp = graph[v];
 		while (tmp) {
 			if (!visited[tmp->vertax]) {
-				printf("%d ", tmp->vertax);
-				visited[tmp->vertax] = 1;
+				printf("%d  ", tmp->vertax);
 				push(tmp->vertax);
+				visited[tmp->vertax] = 1;
 			}
 			tmp = tmp->path;
 		}
 	}
-}
 
+
+}
 
 int main(void) {
 	FILE* fp;
-	int v, e; // vertax & edge
-
 	if (fopen_s(&fp, "input.txt", "r"))
 		exit(1);
+
+	int v, e;
 	fscanf_s(fp, "%d %d", &n, &l);
 	
 	while (fscanf_s(fp, "%d %d", &v, &e) != EOF) {
@@ -92,7 +94,15 @@ int main(void) {
 		adjList(e, v);
 	}
 	printList();
-	memset(visited, 0, MAX_SIZE);
-	DFS(0);
-	return 0;
+	for (int i = 0; i < n; i++) {
+		memset(visited, 0, MAX);
+		DFS(i);
+		printf("\n");
+	}
+
+	printf("\n\n");
+	for (int i = 0; i < n; i++) {
+		BFS(i);
+		printf("\n");
+	}
 }
